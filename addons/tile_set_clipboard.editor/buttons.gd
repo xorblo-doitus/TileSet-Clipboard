846b5ac356bd7baf5a571e51a0a6f44998e15d9c
xorblo-doitus <75997617+xorblo-doitus@users.noreply.github.com>
2025-03-30 08:55:10 +0200

    Fix buttons.tscn uid noise

--- addons/tile_set_clipboard.editor/buttons.gd
+++ addons/tile_set_clipboard.editor/buttons.gd
@@ -2,11 +2,19 @@
 extends HBoxContainer
 
 
+@export_tool_button("Reload Theme", "Reload") var __reload_theme: Callable = load_theme 
+
 @onready var copy_button: Button = %CopyButton
 @onready var paste_button: Button = %PasteButton
 
 
 
 func _ready() -> void:
-	copy_button.icon = EditorInterface.get_base_control().get_theme_icon("ActionCopy", "EditorIcons")
-	paste_button.icon = EditorInterface.get_base_control().get_theme_icon("ActionPaste", "EditorIcons")
+	if EditorInterface.get_base_control().is_ancestor_of(self):
+		load_theme()
+
+
+func load_theme() -> void:
+	var editor_base_control: Control = EditorInterface.get_base_control()
+	copy_button.icon = editor_base_control.get_theme_icon("ActionCopy", "EditorIcons")
+	paste_button.icon = editor_base_control.get_theme_icon("ActionPaste", "EditorIcons")
