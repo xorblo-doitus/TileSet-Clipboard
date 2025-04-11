2740b67334f4e7b3698dd4f260065a3dc30e4c04
xorblo-doitus <75997617+xorblo-doitus@users.noreply.github.com>
2025-04-11 12:49:39 +0200

    Add a dialog to choose if you want to delete this addon's settings

--- addons/tile_set_clipboard.editor/tile_set_clipboard_main.gd
+++ addons/tile_set_clipboard.editor/tile_set_clipboard_main.gd
@@ -21,11 +21,21 @@ func _enable_plugin() -> void:
 	add_shortcut("paste")
 
 func _disable_plugin() -> void:
+	var popup: ConfirmationDialog = ConfirmationDialog.new()
+	popup.dialog_text = "Do you want to also remove editor-persistent data?\nYou may want to answer \"no\" in order to keep your settings for other projects."
+	popup.cancel_button_text = "No"
+	popup.ok_button_text = "Yes"
+	popup.get_cancel_button().grab_focus.call_deferred()
+	popup.confirmed.connect(remove_editor_persistent_data)
+	EditorInterface.popup_dialog_centered(popup)
+
+
+static func remove_editor_persistent_data() -> void:
 	remove_shortcut("copy")
 	remove_shortcut("paste")
 
 
-func add_shortcut(shortcut_name: String) -> void:
+static func add_shortcut(shortcut_name: String) -> void:
 	var setting_path: String = "addons/tile_set_clipboard/shortcuts/" + shortcut_name
 	if !EditorInterface.get_editor_settings().has_setting(setting_path):
 		EditorInterface.get_editor_settings().set_setting(
@@ -38,7 +48,7 @@ func add_shortcut(shortcut_name: String) -> void:
 		)
 
 
-func remove_shortcut(shortcut_name: String) -> void:
+static func remove_shortcut(shortcut_name: String) -> void:
 	EditorInterface.get_editor_settings().set_setting(
 		"addons/tile_set_clipboard/shortcuts/" + shortcut_name,
 		null
